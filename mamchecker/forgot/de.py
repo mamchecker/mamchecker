40adc748f70bec5be62a814a5a2ee2de6b032976
Roland Puntaier <roland.puntaier@gmail.com>
2014-04-21 22:20:55 +0200

    german lang lapse

--- mamchecker/forgot/de.py
+++ mamchecker/forgot/de.py
@@ -3,7 +3,7 @@
 sender_address = "Mamchecker Support <roland.puntaier@gmail.com>"
 subject = "Mamchecker: Password Zurücksetzen"
 body = """
-Bitte klicke auf den folgenden Link, um dich einzuloggen. Ändere dann Passort.
+Bitte klicke auf den folgenden Link, um dich einzuloggen. Ändere dann das Passort.
 
 %s
 """
